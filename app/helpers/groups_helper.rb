01c288fcf1ad86666627f7caaf56a57d69a67c50
Pius <ssekweyamapius@gmail.com>
2023-04-08 15:09:21 +0300

    linters

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,26 +1,26 @@
 module GroupsHelper
   def icon_links
     [
-      "https://cdn-icons-png.flaticon.com/512/2123/2123703.png", # Elephant icon
-      "https://cdn-icons-png.flaticon.com/512/179/179342.png", # Dog icon
-      "https://cdn-icons-png.flaticon.com/512/1216/1216786.png", # Cat icon
-      "https://cdn-icons-png.flaticon.com/512/3174/3174198.png", # Penguin icon
-      "https://cdn-icons-png.flaticon.com/512/2346/2346381.png", # Octopus icon
-      "https://cdn-icons-png.flaticon.com/512/1302/1302404.png", # Butterfly icon
-      "https://cdn-icons-png.flaticon.com/512/4404/4404544.png", # Giraffe icon
-      "https://cdn-icons-png.flaticon.com/512/4645/4645755.png", # Monkey icon
-      "https://cdn-icons-png.flaticon.com/512/1116/1116456.png", # Whale icon
-      "https://cdn-icons-png.flaticon.com/512/2869/2869381.png", # Fox icon
-      "https://cdn-icons-png.flaticon.com/512/3033/3033150.png", # Hamburger icon
-      "https://cdn-icons-png.flaticon.com/512/1397/1397909.png", # Pizza icon
-      "https://cdn-icons-png.flaticon.com/512/3653/3653659.png", # Sushi icon
-      "https://cdn-icons-png.flaticon.com/512/3663/3663605.png", # Ice cream icon
-      "https://cdn-icons-png.flaticon.com/512/834/834882.png", # Coffee icon
-      "https://cdn-icons-png.flaticon.com/512/3261/3261902.png", # Croissant icon
-      "https://cdn-icons-png.flaticon.com/512/2389/2389737.png", # Tacos icon
-      "https://cdn-icons-png.flaticon.com/512/4819/4819582.png", # Ramen icon
-      "https://cdn-icons-png.flaticon.com/512/3898/3898494.png", # Cupcake icon
-      "https://cdn-icons-png.flaticon.com/512/3517/3517218.png", # Donut icon
+      'https://cdn-icons-png.flaticon.com/512/2123/2123703.png', # Elephant icon
+      'https://cdn-icons-png.flaticon.com/512/179/179342.png', # Dog icon
+      'https://cdn-icons-png.flaticon.com/512/1216/1216786.png', # Cat icon
+      'https://cdn-icons-png.flaticon.com/512/3174/3174198.png', # Penguin icon
+      'https://cdn-icons-png.flaticon.com/512/2346/2346381.png', # Octopus icon
+      'https://cdn-icons-png.flaticon.com/512/1302/1302404.png', # Butterfly icon
+      'https://cdn-icons-png.flaticon.com/512/4404/4404544.png', # Giraffe icon
+      'https://cdn-icons-png.flaticon.com/512/4645/4645755.png', # Monkey icon
+      'https://cdn-icons-png.flaticon.com/512/1116/1116456.png', # Whale icon
+      'https://cdn-icons-png.flaticon.com/512/2869/2869381.png', # Fox icon
+      'https://cdn-icons-png.flaticon.com/512/3033/3033150.png', # Hamburger icon
+      'https://cdn-icons-png.flaticon.com/512/1397/1397909.png', # Pizza icon
+      'https://cdn-icons-png.flaticon.com/512/3653/3653659.png', # Sushi icon
+      'https://cdn-icons-png.flaticon.com/512/3663/3663605.png', # Ice cream icon
+      'https://cdn-icons-png.flaticon.com/512/834/834882.png', # Coffee icon
+      'https://cdn-icons-png.flaticon.com/512/3261/3261902.png', # Croissant icon
+      'https://cdn-icons-png.flaticon.com/512/2389/2389737.png', # Tacos icon
+      'https://cdn-icons-png.flaticon.com/512/4819/4819582.png', # Ramen icon
+      'https://cdn-icons-png.flaticon.com/512/3898/3898494.png', # Cupcake icon
+      'https://cdn-icons-png.flaticon.com/512/3517/3517218.png' # Donut icon
     ]
   end
 end
